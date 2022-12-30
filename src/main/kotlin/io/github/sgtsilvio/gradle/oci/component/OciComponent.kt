c818a7f7e80ad70fabbd69bb3ca659a5deb45f36
Silvio Giebl <gieblsilvio@outlook.com>
2022-12-30 20:57:16 +0100

    Make OciComponent serializable

--- src/main/kotlin/io/github/sgtsilvio/gradle/oci/component/OciComponent.kt
+++ src/main/kotlin/io/github/sgtsilvio/gradle/oci/component/OciComponent.kt
@@ -1,6 +1,7 @@
 package io.github.sgtsilvio.gradle.oci.component
 
 import io.github.sgtsilvio.gradle.oci.internal.calculateSha256Digest
+import java.io.Serializable
 import java.time.Instant
 
 /**
@@ -10,13 +11,13 @@ data class OciComponent(
     val capabilities: Set<Capability>,
     val bundleOrPlatformBundles: BundleOrPlatformBundles,
     val indexAnnotations: Map<String, String>,
-) {
+) : Serializable {
 
-    data class Capability(val group: String, val name: String)
+    data class Capability(val group: String, val name: String) : Serializable
 
     sealed interface BundleOrPlatformBundles
 
-    data class PlatformBundles(val map: Map<Platform, Bundle>) : BundleOrPlatformBundles
+    data class PlatformBundles(val map: Map<Platform, Bundle>) : BundleOrPlatformBundles, Serializable
 
     data class Platform(
         val architecture: String,
@@ -24,7 +25,7 @@ data class OciComponent(
         val osVersion: String?,
         val osFeatures: List<String>,
         val variant: String?,
-    )
+    ) : Serializable
 
     data class Bundle(
         val creationTime: Instant?,
@@ -42,12 +43,12 @@ data class OciComponent(
         val manifestDescriptorAnnotations: Map<String, String>,
         val parentCapabilities: List<Capability>,
         val layers: List<Layer>,
-    ) : BundleOrPlatformBundles {
+    ) : BundleOrPlatformBundles, Serializable {
 
         data class Command(
             val entryPoint: List<String>?, // empty (no args) is different from null (not set, inherit)
             val arguments: List<String> // default empty
-        )
+        ) : Serializable
 
         data class Layer(
             val descriptor: Descriptor?,
@@ -55,14 +56,14 @@ data class OciComponent(
             val author: String?,
             val createdBy: String?,
             val comment: String?,
-        ) {
+        ) : Serializable {
 
             data class Descriptor(
                 override val digest: String,
                 val diffId: String,
                 override val size: Long,
                 override val annotations: Map<String, String>,
-            ) : OciComponent.Descriptor
+            ) : OciComponent.Descriptor, Serializable
         }
     }
 
