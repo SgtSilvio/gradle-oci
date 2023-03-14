a0c7f7913fb6c0aa26b12330a233c8e03cb2a53b
Silvio Giebl <gieblsilvio@outlook.com>
2023-03-14 19:11:01 +0100

    Extract PlatformSet from OciComponentResolver

---
+++ src/main/kotlin/io/github/sgtsilvio/gradle/oci/component/PlatformSet.kt
@@ -0,0 +1,48 @@
+package io.github.sgtsilvio.gradle.oci.component
+
+import io.github.sgtsilvio.gradle.oci.platform.Platform
+
+class PlatformSet : Iterable<Platform> {
+    var isInfinite: Boolean private set
+    private val set = hashSetOf<Platform>()
+
+    constructor(isInfinite: Boolean) {
+        this.isInfinite = isInfinite
+    }
+
+    constructor(platform: Platform) {
+        isInfinite = false
+        set.add(platform)
+    }
+
+    fun intersect(other: PlatformSet) {
+        if (other.isInfinite) {
+            return
+        }
+        if (isInfinite) {
+            isInfinite = false
+            set.addAll(other.set)
+        } else {
+            set.retainAll((other.set))
+        }
+    }
+
+    fun unionise(other: PlatformSet) {
+        if (isInfinite) {
+            return
+        }
+        if (other.isInfinite) {
+            isInfinite = true
+            set.clear()
+        } else {
+            set.addAll(other.set)
+        }
+    }
+
+    override fun iterator(): Iterator<Platform> {
+        if (isInfinite) {
+            throw UnsupportedOperationException("iterating an infinite set is not possible")
+        }
+        return set.iterator()
+    }
+}
