0ce92086d267eaaffecfc4cd0882dcaa16abcef3
Silvio Giebl <gieblsilvio@outlook.com>
2023-09-11 12:08:33 +0200

    Add signing plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `kotlin-dsl`
+    signing
     alias(libs.plugins.pluginPublish)
     alias(libs.plugins.defaults)
     alias(libs.plugins.metadata)
@@ -61,6 +62,12 @@ gradlePlugin {
             tags.set(listOf("oci", "oci-image", "docker", "multi-arch"))
         }
     }
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
 }
 
 testing {
