cddd21972bfb500a54ad5e697dd5e3a9acd76fd5
Silvio Giebl <gieblsilvio@outlook.com>
2023-12-10 16:17:27 +0100

    Cleanup ProviderExtensions

--- src/main/kotlin/io/github/sgtsilvio/gradle/oci/internal/gradle/ProviderExtensions.kt
+++ src/main/kotlin/io/github/sgtsilvio/gradle/oci/internal/gradle/ProviderExtensions.kt
@@ -3,15 +3,15 @@ package io.github.sgtsilvio.gradle.oci.internal.gradle
 import org.gradle.api.provider.Provider
 import java.util.*
 
-internal fun <T, B, R> Provider<T>.zipAbsentAsNull(other: Provider<B>, combiner: (T, B?) -> R): Provider<R> =
-    zip(other.map { Optional.ofNullable(it) }.orElse(Optional.empty())) { t, b -> combiner(t, b.orElse(null)) }
+internal fun <T, U, R> Provider<T>.zipAbsentAsNull(other: Provider<U>, combiner: (T, U?) -> R): Provider<R> =
+    zip(other.map { Optional.ofNullable(it) }.orElse(Optional.empty())) { t, u -> combiner(t, u.orElse(null)) }
 
-internal fun <T, B, R> Provider<T>.zipAbsentAsEmptySet(
-    other: Provider<Set<B>>,
-    combiner: (T, Set<B>) -> R,
-): Provider<R> = zip(other.orElse(setOf())) { t, b -> combiner(t, b) }
+internal fun <T, E, R> Provider<T>.zipAbsentAsEmptySet(
+    other: Provider<Set<E>>,
+    combiner: (T, Set<E>) -> R,
+): Provider<R> = zip(other.orElse(setOf())) { t, set -> combiner(t, set) }
 
 internal fun <T, K, V, R> Provider<T>.zipAbsentAsEmptyMap(
     other: Provider<Map<K, V>>,
     combiner: (T, Map<K, V>) -> R,
-): Provider<R> = zip(other.orElse(mapOf())) { t, b -> combiner(t, b) }
+): Provider<R> = zip(other.orElse(mapOf())) { t, map -> combiner(t, map) }
