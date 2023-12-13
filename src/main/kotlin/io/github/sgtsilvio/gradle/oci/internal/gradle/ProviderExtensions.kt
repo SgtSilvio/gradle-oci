1d16a40a446a16b51c4da85191ed2c16974ccb1a
Silvio Giebl <gieblsilvio@outlook.com>
2023-12-13 22:38:27 +0100

    Cleanup ProviderExtensions

--- src/main/kotlin/io/github/sgtsilvio/gradle/oci/internal/gradle/ProviderExtensions.kt
+++ src/main/kotlin/io/github/sgtsilvio/gradle/oci/internal/gradle/ProviderExtensions.kt
@@ -3,7 +3,8 @@ package io.github.sgtsilvio.gradle.oci.internal.gradle
 import org.gradle.api.provider.Provider
 import java.util.*
 
-internal fun <T> Provider<T>.optional() = map { Optional.ofNullable(it) }.orElse(Optional.empty())
+internal fun <T> Provider<T>.optional(): Provider<Optional<T & Any>> =
+    map { Optional.ofNullable(it) }.orElse(Optional.empty())
 
 internal fun <T, U, R> Provider<T>.zipAbsentAsNull(other: Provider<U>, combiner: (T, U?) -> R): Provider<R> =
     zip(other.optional()) { t, u -> combiner(t, u.orElse(null)) }
