54714cf41356c5e46474bb7dc19e8724841251bf
Silvio Giebl <gieblsilvio@outlook.com>
2024-03-28 21:44:31 +0100

    Cleanup examples

--- examples/java-app-groovy-dsl/src/test/java/org/example/java/app/ImageTest.java
+++ examples/java-app-groovy-dsl/src/test/java/org/example/java/app/ImageTest.java
@@ -9,7 +9,7 @@ class ImageTest {
 
     @Test
     void start() {
-        try (final GenericContainer<?> container = new GenericContainer(OciImages.getImageName("example/example-java-app"))) {
+        try (final GenericContainer<?> container = new GenericContainer<>(OciImages.getImageName("example/example-java-app"))) {
             container.waitingFor(Wait.forLogMessage("Hello World!\n", 1));
             container.withLogConsumer(outputFrame -> System.out.println(outputFrame.getUtf8StringWithoutLineEnding()));
             container.start();
