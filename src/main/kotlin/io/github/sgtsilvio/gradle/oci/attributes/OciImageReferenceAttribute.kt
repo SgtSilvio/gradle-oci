6d77915cba92985cf7a4709f5d29e4d0b441765d
Silvio Giebl <gieblsilvio@outlook.com>
2024-07-09 10:57:23 +0200

    WIP variants for each platform - Reduce visibility to internal as possible

--- src/main/kotlin/io/github/sgtsilvio/gradle/oci/attributes/OciImageReferenceAttribute.kt
+++ src/main/kotlin/io/github/sgtsilvio/gradle/oci/attributes/OciImageReferenceAttribute.kt
@@ -2,5 +2,5 @@ package io.github.sgtsilvio.gradle.oci.attributes
 
 import org.gradle.api.attributes.Attribute
 
-val OCI_IMAGE_REFERENCE_ATTRIBUTE: Attribute<String> =
+internal val OCI_IMAGE_REFERENCE_ATTRIBUTE: Attribute<String> =
     Attribute.of("io.github.sgtsilvio.gradle.oci.image.reference", String::class.java)
--- src/main/kotlin/io/github/sgtsilvio/gradle/oci/attributes/PlatformAttribute.kt
+++ src/main/kotlin/io/github/sgtsilvio/gradle/oci/attributes/PlatformAttribute.kt
@@ -9,7 +9,7 @@ val PLATFORM_ATTRIBUTE: Attribute<String> = Attribute.of("io.github.sgtsilvio.gr
 const val UNIVERSAL_PLATFORM_ATTRIBUTE_VALUE = "universal"
 const val MULTIPLE_PLATFORMS_ATTRIBUTE_VALUE = "multiple"
 
-class PlatformAttributeCompatibilityRule : AttributeCompatibilityRule<String> {
+internal class PlatformAttributeCompatibilityRule : AttributeCompatibilityRule<String> {
     override fun execute(details: CompatibilityCheckDetails<String>) {
         if (details.producerValue == UNIVERSAL_PLATFORM_ATTRIBUTE_VALUE) {
             details.compatible()
