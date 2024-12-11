fabd49f4ed6ddf407f15c29c081ad7f32245b6bd
Silvio Giebl <gieblsilvio@outlook.com>
2024-12-11 14:09:55 +0100

    Add DockerLayerTask WIP

--- src/main/kotlin/io/github/sgtsilvio/gradle/oci/OciLayerTask.kt
+++ src/main/kotlin/io/github/sgtsilvio/gradle/oci/OciLayerTask.kt
@@ -24,11 +24,6 @@ import java.util.zip.GZIPOutputStream
  */
 abstract class OciLayerTask : DefaultTask() {
 
-    private val _contents = project.objects.newOciCopySpec()
-
-    @get:Nested
-    protected val copySpecInput = _contents.asInput(project.providers)
-
     @get:Input
     val digestAlgorithm: Property<OciDigestAlgorithm> =
         project.objects.property<OciDigestAlgorithm>().convention(OciDigestAlgorithm.SHA_256)
@@ -73,14 +68,8 @@ abstract class OciLayerTask : DefaultTask() {
         diffId = properties.map { it.getProperty("diffId").toOciDigest() }
     }
 
-    @get:Internal
-    val contents: OciCopySpec get() = _contents
-
-    fun contents(action: Action<in OciCopySpec>) = action.execute(_contents)
-
     @TaskAction
     protected fun run() {
-        val copySpecInput = copySpecInput.get()
         val digestAlgorithm = digestAlgorithm.get()
         val compression = compression.get()
         val file = file.get().asFile
@@ -92,37 +81,56 @@ abstract class OciLayerTask : DefaultTask() {
                 TarArchiveOutputStream(dos, StandardCharsets.UTF_8.name()).use { tos ->
                     tos.setLongFileMode(TarArchiveOutputStream.LONGFILE_POSIX)
                     tos.setBigNumberMode(TarArchiveOutputStream.BIGNUMBER_POSIX)
-                    copySpecInput.process(object : OciCopySpecVisitor {
-                        override fun visitFile(fileMetadata: FileMetadata, fileSource: FileSource) {
-                            tos.putArchiveEntry(TarArchiveEntry(fileMetadata.path).apply {
-                                setPermissions(fileMetadata.permissions)
-                                setUserId(fileMetadata.userId)
-                                setGroupId(fileMetadata.groupId)
-                                setModTime(fileMetadata.modificationTime.toEpochMilli())
-                                size = fileMetadata.size
-                            })
-                            fileSource.copyTo(tos)
-                            tos.closeArchiveEntry()
-                        }
-
-                        override fun visitDirectory(fileMetadata: FileMetadata) {
-                            tos.putArchiveEntry(TarArchiveEntry(fileMetadata.path).apply {
-                                setPermissions(fileMetadata.permissions)
-                                setUserId(fileMetadata.userId)
-                                setGroupId(fileMetadata.groupId)
-                                setModTime(fileMetadata.modificationTime.toEpochMilli())
-                            })
-                            tos.closeArchiveEntry()
-                        }
-
-                        private fun TarArchiveEntry.setPermissions(permissions: Int) {
-                            mode = (mode and 0b111_111_111.inv()) or (permissions and 0b111_111_111)
-                        }
-                    })
+                    run(tos)
                 }
             }
         }
         propertiesFile.writeText("digest=$digest\nsize=${file.length()}\ndiffId=$diffId")
+    }
+
+    protected abstract fun run(tos: TarArchiveOutputStream)
+}
+
+abstract class DefaultOciLayerTask : OciLayerTask() {
+
+    private val _contents = project.objects.newOciCopySpec()
+
+    @get:Nested
+    protected val copySpecInput = _contents.asInput(project.providers)
+
+    @get:Internal
+    val contents: OciCopySpec get() = _contents
+
+    fun contents(action: Action<in OciCopySpec>) = action.execute(_contents)
+
+    override fun run(tos: TarArchiveOutputStream) {
+        copySpecInput.get().process(object : OciCopySpecVisitor {
+            override fun visitFile(fileMetadata: FileMetadata, fileSource: FileSource) {
+                tos.putArchiveEntry(TarArchiveEntry(fileMetadata.path).apply {
+                    setPermissions(fileMetadata.permissions)
+                    setUserId(fileMetadata.userId)
+                    setGroupId(fileMetadata.groupId)
+                    setModTime(fileMetadata.modificationTime.toEpochMilli())
+                    size = fileMetadata.size
+                })
+                fileSource.copyTo(tos)
+                tos.closeArchiveEntry()
+            }
+
+            override fun visitDirectory(fileMetadata: FileMetadata) {
+                tos.putArchiveEntry(TarArchiveEntry(fileMetadata.path).apply {
+                    setPermissions(fileMetadata.permissions)
+                    setUserId(fileMetadata.userId)
+                    setGroupId(fileMetadata.groupId)
+                    setModTime(fileMetadata.modificationTime.toEpochMilli())
+                })
+                tos.closeArchiveEntry()
+            }
+
+            private fun TarArchiveEntry.setPermissions(permissions: Int) {
+                mode = (mode and 0b111_111_111.inv()) or (permissions and 0b111_111_111)
+            }
+        })
     }
 }
 
