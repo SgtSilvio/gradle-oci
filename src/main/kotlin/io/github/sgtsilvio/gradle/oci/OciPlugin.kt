c87ebf877f10ebc43e8a4d7144f3d6a614993575
Silvio Giebl <gieblsilvio@outlook.com>
2024-12-18 23:50:17 +0100

    Refactor OciPlugin task registration

--- src/main/kotlin/io/github/sgtsilvio/gradle/oci/OciPlugin.kt
+++ src/main/kotlin/io/github/sgtsilvio/gradle/oci/OciPlugin.kt
@@ -2,6 +2,7 @@ package io.github.sgtsilvio.gradle.oci
 
 import io.github.sgtsilvio.gradle.oci.attributes.*
 import io.github.sgtsilvio.gradle.oci.dsl.OciExtension
+import io.github.sgtsilvio.gradle.oci.dsl.OciImageDefinition
 import io.github.sgtsilvio.gradle.oci.image.LoadOciImagesTask
 import io.github.sgtsilvio.gradle.oci.image.PushOciImageTask
 import io.github.sgtsilvio.gradle.oci.internal.dsl.OciExtensionImpl
@@ -30,29 +31,35 @@ class OciPlugin : Plugin<Project> {
             attribute(OCI_IMAGE_REFERENCE_SPECS_ATTRIBUTE)
         }
         val extension = project.extensions.create(OciExtension::class, EXTENSION_NAME, OciExtensionImpl::class)
-        registerPushTasks(project, extension)
+        registerImageTasks(project, extension)
     }
 
-    private fun registerPushTasks(project: Project, extension: OciExtension) {
+    private fun registerImageTasks(project: Project, extension: OciExtension) {
         extension.imageDefinitions.all {
-            val imageDefName = name
-            val pushName = "push".concatCamelCase(imageDefName.mainToEmpty())
-            project.tasks.register<PushOciImageTask>(pushName.concatCamelCase("ociImage")) {
-                group = TASK_GROUP_NAME
-                description = "Pushes the '$imageDefName' OCI image to a registry."
-                from(extension.imageDependencies.create(pushName).apply {
-                    runtime(dependency).name(imageName).tag(imageTags)
-                })
-            }
+            registerPushTask(this, project, extension)
+            registerLoadTask(this, project, extension)
+        }
+    }
 
-            val loadName = "load".concatCamelCase(imageDefName.mainToEmpty())
-            project.tasks.register<LoadOciImagesTask>(loadName.concatCamelCase("ociImage")) {
-                group = TASK_GROUP_NAME
-                description = "Loads the '$imageDefName' OCI image to the Docker daemon."
-                from(extension.imageDependencies.create(loadName).apply {
-                    runtime(dependency).name(imageName).tag(imageTags)
-                })
-            }
+    private fun registerPushTask(imageDefinition: OciImageDefinition, project: Project, extension: OciExtension) {
+        val pushName = "push".concatCamelCase(imageDefinition.name.mainToEmpty())
+        project.tasks.register<PushOciImageTask>(pushName.concatCamelCase("ociImage")) {
+            group = TASK_GROUP_NAME
+            description = "Pushes the '${imageDefinition.name}' OCI image to a registry."
+            from(extension.imageDependencies.create(pushName).apply {
+                runtime(imageDefinition.dependency).name(imageName).tag(imageTags)
+            })
+        }
+    }
+
+    private fun registerLoadTask(imageDefinition: OciImageDefinition, project: Project, extension: OciExtension) {
+        val loadName = "load".concatCamelCase(imageDefinition.name.mainToEmpty())
+        project.tasks.register<LoadOciImagesTask>(loadName.concatCamelCase("ociImage")) {
+            group = TASK_GROUP_NAME
+            description = "Loads the '${imageDefinition.name}' OCI image to the Docker daemon."
+            from(extension.imageDependencies.create(loadName).apply {
+                runtime(imageDefinition.dependency).name(imageName).tag(imageTags)
+            })
         }
     }
 }
