3b477fbe9f466e756b5800b377bb81c0f9c9bab6
Silvio Giebl <gieblsilvio@outlook.com>
2025-01-13 00:31:36 +0100

    Improve http server configuration in LoadOciImagesTask

--- src/main/kotlin/io/github/sgtsilvio/gradle/oci/image/LoadOciImagesTask.kt
+++ src/main/kotlin/io/github/sgtsilvio/gradle/oci/image/LoadOciImagesTask.kt
@@ -1,10 +1,13 @@
 package io.github.sgtsilvio.gradle.oci.image
 
 import io.github.sgtsilvio.gradle.oci.internal.gradle.redirectOutput
+import io.github.sgtsilvio.gradle.oci.internal.reactor.netty.OciLoopResources
 import io.github.sgtsilvio.gradle.oci.metadata.OciDigest
 import io.github.sgtsilvio.gradle.oci.metadata.OciImageReference
 import io.github.sgtsilvio.oci.registry.DistributionRegistryStorage
 import io.github.sgtsilvio.oci.registry.OciRegistryHandler
+import io.netty.buffer.UnpooledByteBufAllocator
+import io.netty.channel.ChannelOption
 import org.apache.commons.lang3.SystemUtils
 import org.gradle.process.ExecOperations
 import org.gradle.work.DisableCachingByDefault
@@ -31,28 +34,36 @@ abstract class LoadOciImagesTask @Inject constructor(private val execOperations:
             registryDataDirectory,
         )
         val host = if (SystemUtils.IS_OS_WINDOWS || SystemUtils.IS_OS_MAC) "host.docker.internal" else "localhost"
-        val httpServer =
-            HttpServer.create().handle(OciRegistryHandler(DistributionRegistryStorage(registryDataDirectory))).bindNow()
+        val loopResources = OciLoopResources.acquire()
         try {
-            for ((_, imageReferences) in multiPlatformImageAndReferencesPairs) {
-                for (imageReference in imageReferences) {
-                    val registryImageReference = "$host:${httpServer.port()}/$imageReference"
-                    execOperations.exec {
-                        commandLine("docker", "pull", registryImageReference)
-                        redirectOutput(logger)
-                    }
-                    execOperations.exec {
-                        commandLine("docker", "tag", registryImageReference, imageReference)
-                        redirectOutput(logger)
-                    }
-                    execOperations.exec {
-                        commandLine("docker", "rmi", registryImageReference)
-                        redirectOutput(logger)
+            val httpServer = HttpServer.create()
+                .runOn(loopResources)
+                .childOption(ChannelOption.ALLOCATOR, UnpooledByteBufAllocator.DEFAULT)
+                .handle(OciRegistryHandler(DistributionRegistryStorage(registryDataDirectory)))
+                .bindNow()
+            try {
+                for ((_, imageReferences) in multiPlatformImageAndReferencesPairs) {
+                    for (imageReference in imageReferences) {
+                        val registryImageReference = "$host:${httpServer.port()}/$imageReference"
+                        execOperations.exec {
+                            commandLine("docker", "pull", registryImageReference)
+                            redirectOutput(logger)
+                        }
+                        execOperations.exec {
+                            commandLine("docker", "tag", registryImageReference, imageReference)
+                            redirectOutput(logger)
+                        }
+                        execOperations.exec {
+                            commandLine("docker", "rmi", registryImageReference)
+                            redirectOutput(logger)
+                        }
                     }
                 }
+            } finally {
+                httpServer.disposeNow()
             }
         } finally {
-            httpServer.disposeNow()
+            OciLoopResources.release()
         }
         registryDataDirectory.toFile().deleteRecursively()
     }
