2dec67a69e268eb305d18773b68b679dc5f26ed0
Silvio Giebl <gieblsilvio@outlook.com>
2025-01-27 14:02:00 +0100

    Create tar tasks for every oci image layout task

--- src/main/kotlin/io/github/sgtsilvio/gradle/oci/OciPlugin.kt
+++ src/main/kotlin/io/github/sgtsilvio/gradle/oci/OciPlugin.kt
@@ -5,6 +5,7 @@ import io.github.sgtsilvio.gradle.oci.dsl.OciExtension
 import io.github.sgtsilvio.gradle.oci.dsl.OciImageDefinition
 import io.github.sgtsilvio.gradle.oci.image.LoadOciImageTask
 import io.github.sgtsilvio.gradle.oci.image.OciImageLayoutTask
+import io.github.sgtsilvio.gradle.oci.image.OciImagesLayoutTask
 import io.github.sgtsilvio.gradle.oci.image.PushOciImageTask
 import io.github.sgtsilvio.gradle.oci.internal.createOciImageLayoutClassifier
 import io.github.sgtsilvio.gradle.oci.internal.dsl.OciExtensionImpl
@@ -13,9 +14,11 @@ import io.github.sgtsilvio.gradle.oci.internal.string.camelCase
 import io.github.sgtsilvio.gradle.oci.internal.string.concatCamelCase
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.UnknownTaskException
 import org.gradle.api.tasks.bundling.Tar
 import org.gradle.kotlin.dsl.add
 import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.register
 
 /**
@@ -42,8 +45,9 @@ class OciPlugin : Plugin<Project> {
         extension.imageDefinitions.all {
             registerPushTask(this, project, extension)
             registerLoadTask(this, project, extension)
-            registerLayoutTasks(this, project, extension)
+            registerLayoutTask(this, project, extension)
         }
+        registerLayoutTarTaskRule(project)
     }
 
     private fun registerPushTask(imageDefinition: OciImageDefinition, project: Project, extension: OciExtension) {
@@ -68,9 +72,9 @@ class OciPlugin : Plugin<Project> {
         }
     }
 
-    private fun registerLayoutTasks(imageDefinition: OciImageDefinition, project: Project, extension: OciExtension) {
+    private fun registerLayoutTask(imageDefinition: OciImageDefinition, project: Project, extension: OciExtension) {
         val imageLayoutClassifier = createOciImageLayoutClassifier(imageDefinition.name)
-        val imageLayoutTask = project.tasks.register<OciImageLayoutTask>(imageLayoutClassifier.camelCase()) {
+        project.tasks.register<OciImageLayoutTask>(imageLayoutClassifier.camelCase()) {
             group = TASK_GROUP_NAME
             description = "Creates an OCI image layout directory for the '${imageDefinition.name}' OCI image."
             from(extension.imageDependencies.create(name).apply {
@@ -79,12 +83,23 @@ class OciPlugin : Plugin<Project> {
             destinationDirectory.set(project.layout.buildDirectory.dir("oci/images/${imageDefinition.name}"))
             classifier.set(imageLayoutClassifier)
         }
-        project.tasks.register<Tar>(imageLayoutTask.name.concatCamelCase("tar")) {
-            group = TASK_GROUP_NAME
-            description = "Creates an OCI image layout tar for the '${imageDefinition.name}' OCI image."
-            from(imageLayoutTask)
-            destinationDirectory.set(imageLayoutTask.flatMap { it.destinationDirectory })
-            archiveClassifier.set(imageLayoutTask.flatMap { it.classifier })
+    }
+
+    private fun registerLayoutTarTaskRule(project: Project) {
+        project.tasks.addRule("Pattern: <ociImageLayoutTaskName>Tar") {
+            if (endsWith("Tar")) {
+                try {
+                    val imageLayoutTask = project.tasks.named<OciImagesLayoutTask>(removeSuffix("Tar"))
+                    project.tasks.register<Tar>(imageLayoutTask.name.concatCamelCase("tar")) {
+                        group = TASK_GROUP_NAME
+                        description = "Creates a tar of the OCI image layout created by task '${imageLayoutTask.name}'."
+                        from(imageLayoutTask)
+                        destinationDirectory.set(imageLayoutTask.flatMap { it.destinationDirectory })
+                        archiveClassifier.set(imageLayoutTask.flatMap { it.classifier })
+                    }
+                } catch (ignored: UnknownTaskException) {
+                }
+            }
         }
     }
 }
