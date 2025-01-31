9d87a4c4be6d0708e0ab107a974bbe1e98e42c5c
Silvio Giebl <gieblsilvio@outlook.com>
2025-01-31 21:37:52 +0100

    Use custom executable path search only on unix systems

--- src/main/kotlin/io/github/sgtsilvio/gradle/oci/internal/ProcessUtil.kt
+++ src/main/kotlin/io/github/sgtsilvio/gradle/oci/internal/ProcessUtil.kt
@@ -1,21 +1,24 @@
 package io.github.sgtsilvio.gradle.oci.internal
 
+import org.apache.commons.lang3.SystemUtils
 import java.io.File
 import kotlin.io.path.Path
 import kotlin.io.path.exists
 import kotlin.io.path.isDirectory
 
 internal fun findExecutablePath(name: String): String {
-    val pathEnvVar = System.getenv("PATH") ?: return name
-    val searchPaths = pathEnvVar.split(File.pathSeparatorChar)
-    for (searchPath in searchPaths) {
-        val path = try {
-            Path(searchPath, name)
-        } catch (ignored: IllegalArgumentException) {
-            continue
-        }
-        if (path.exists() && !path.isDirectory()) {
-            return path.toString()
+    if (SystemUtils.IS_OS_UNIX) {
+        val pathEnvVar = System.getenv("PATH") ?: return name
+        val searchPaths = pathEnvVar.split(File.pathSeparatorChar)
+        for (searchPath in searchPaths) {
+            val path = try {
+                Path(searchPath, name)
+            } catch (ignored: IllegalArgumentException) {
+                continue
+            }
+            if (path.exists() && !path.isDirectory()) {
+                return path.toString()
+            }
         }
     }
     return name
